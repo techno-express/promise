9544c17322ab0d57002b6b726a6c950efdd39908
techno-express <technoexpressnet@gmail.com>
2018-12-08 19:47:34 -0500

    returns the promise state

--- src/Promise.php
+++ src/Promise.php
@@ -5,7 +5,8 @@ namespace React\Promise;
 final class Promise implements PromiseInterface
 {
     private $canceller;
-    private $result;
+    private $result = null;
+    private $state = 'pending';
 
     private $handlers = [];
 
@@ -208,5 +209,53 @@ final class Promise implements PromiseInterface
         } catch (\Exception $e) {
             $this->reject($e);
         }
+    }
+	
+    /**
+     * Returns the promise state.
+     *
+     *  'pending'	The promise is still open
+     *  'fulfilled'	The promise completed successfully
+     *  'rejected'	The promise failed
+     *
+     * @return string A promise state
+     */
+	public function getState()
+	{
+		if ($this->isPending())
+			$this->state = 'pending';
+		elseif ($this->isFulfilled())
+			$this->state = 'fulfilled';
+		elseif ($this->isRejected())
+			$this->state = 'rejected';
+		
+		return $this->state;
+	}
+	
+    /**
+     * @override
+     * @inheritDoc
+     */
+    public function isPending()
+    {
+        return $this->result === null;
+    }
+
+    /**
+     * @override
+     * @inheritDoc
+     */
+    public function isFulfilled()
+    {
+        return !$this->isPending() && $this->result->isFulfilled();
+    }
+
+    /**
+     * @override
+     * @inheritDoc
+     */
+    public function isRejected()
+    {
+        return !$this->isPending() && $this->result->isRejected();
     }
 }
